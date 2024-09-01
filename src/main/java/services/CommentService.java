5dfeac969b5568c79b4f356434494a0cc171c7b3
Amin Tazifor <amin.tazifor@gmail.com>
2024-09-01 13:36:07 +0100

    stereotype prototype beans

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,6 +1,8 @@
 package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import repositories.CommentRepository;
 
